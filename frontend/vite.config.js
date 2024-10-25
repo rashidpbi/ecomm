bd8e2aedada74a03d84d14678b79e94b4f0aa986
rashidpbi <66490202+rashidpbi@users.noreply.github.com>
2024-10-25 14:50:53 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,12 +5,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-  },
-  build: {
-    rollupOptions: {
-      external: [
-        'react-router-dom',
-      ],
-    },
-  },
+  }
 });
